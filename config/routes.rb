ffc8389f9be640a2eb0efdf788749ded031d2dd2
Pablo <ezekielgonzalezr@gmail.com>
2023-09-04 20:07:42 -0600

    Add user index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :users, only: :index
   resources :foods, only: [:new, :create, :show, :destroy ]
   resources :recipes, only: [:new, :create, :show, :destroy ]
   root "foods#index"
