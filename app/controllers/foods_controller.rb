bbaba428ed05b7ae04fc95494965fa51491b7a3e
Ezra <ezraermy@gmail.com>
2023-09-05 17:59:26 +0300

    Generate a shopping list and create a form for new and edit foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,6 +9,22 @@ class FoodsController < ApplicationController
     # Show action to display details of a specific food item.
     def show
     end
+
+    # Custom action to generate a general shopping list summary.
+    def general_shopping_list
+        @foods = Food.all
+        @food_items = @foods.length
+        @price_sum = Food.sum(:price)
+      end
+    
+      # New action to render a form for creating a new food item.
+      def new
+        @food = Food.new
+      end
+    
+      # Edit action to render a form for editing a food item.
+      def edit
+      end
     
   end
   
