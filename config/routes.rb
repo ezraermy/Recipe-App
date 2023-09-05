142a2378325a1903e0de618b5a8a79106595be49
Ezra <ezraermy@gmail.com>
2023-09-05 22:03:11 +0300

    Update food routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: :index
-  resources :foods, only: [:new, :create, :show, :destroy ]
+  resources :foods
   resources :recipes, only: [:new, :create, :show, :destroy ]
 
   get '/general_shoping_list', to: 'foods#general_shoping_list'
