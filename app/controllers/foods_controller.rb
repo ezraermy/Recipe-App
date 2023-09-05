ede81d2d34291d98c1902fb6d1125907b55162c4
Ezra <ezraermy@gmail.com>
2023-09-05 17:58:07 +0300

    Display a list of foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,14 @@
 class FoodsController < ApplicationController
+    before_action :set_food, only: %i[show edit update destroy]
+  
+    # Index action to display a list of food items.
+    def index
+      @foods = Food.includes(:user).all
+    end
+  
+    # Show action to display details of a specific food item.
+    def show
+    end
     
   end
   
