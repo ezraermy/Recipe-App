8bf19cb55632548655033bb5bfd576e949cfc695
Ezra <ezraermy@gmail.com>
2023-09-05 22:02:31 +0300

    Update food and food_form view

--- db/migrate/20230904205732_create_foods.rb
+++ db/migrate/20230904205732_create_foods.rb
@@ -1,15 +1,12 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.string :name, null: false
-      t.string :measurement_unit, null: false
-      t.decimal :price, precision: 10, scale: 2, null: false
-      t.integer :quantity, null: false
-      t.references :user, foreign_key: true
+      t.string :name
+      t.string :measurement_unit
+      t.float :price
+      t.integer :quantity
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
-    end
-
-    add_index :foods, [:name, :user_id], unique: true
   end
 end
