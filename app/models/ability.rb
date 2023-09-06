fb1cac27c55f36508e029b63ac37aff3f0405bbd
Pablo <ezekielgonzalezr@gmail.com>
2023-09-06 10:34:47 -0600

    Fix linter

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,10 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-      return unless user.present?
-      can :read, Recipe, public: true
-      can :manage, Recipe, :user_id => user.id
+    return unless user.present?
+
+    can :read, Recipe, public: true
+    can :manage, Recipe, user_id: user.id
   end
 end
