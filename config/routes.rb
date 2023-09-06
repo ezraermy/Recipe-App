9b4e5e57c48739cf06486b499ea7ba8d5f75e2d7
Pablo <ezekielgonzalezr@gmail.com>
2023-09-05 22:08:32 -0600

    Add switch for visibility

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, only: :index
   resources :foods, only: [:index, :new, :create, :show, :destroy ]
-  resources :recipes, only: [:index, :new, :create, :show, :destroy ]
+  resources :recipes, only: [:index, :update, :new, :create, :show, :destroy ]
 
   get '/general_shoping_list', to: 'foods#general_shoping_list'
   get '/public_recipes', to: 'recipes#public_recipes'
