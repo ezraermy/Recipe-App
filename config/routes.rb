a37d96ce3ddb3a9abc5b60aba053d123f1c432fe
Pablo <ezekielgonzalezr@gmail.com>
2023-09-09 21:08:15 -0600

    Update user index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users, only: :index
+  get '/user_account', to: 'users#index'
   resources :foods, only: [:index, :new, :create, :show, :destroy ]
   resources :recipes, only: [:index, :update, :new, :create, :show, :destroy ] do
   resources :recipe_foods, only: [:update, :new, :create, :destroy ]
